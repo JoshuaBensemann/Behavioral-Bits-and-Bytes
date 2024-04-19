35c89fdc544169335e7f087e2f2b86beaff50298
Joshua Bensemann <joshuabensemann@gmail.com>
2024-04-19 15:49:52 +1200

    typo

--- pigeons_as_classifiers/PAC/helpers.py
+++ pigeons_as_classifiers/PAC/helpers.py
@@ -1,8 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
 from .data import get_testing_dataset
 from .eval import evaluate_model
-import pandas as pd
-import numpy as np
-from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
 
 
 def run_evaluation(
@@ -25,28 +26,38 @@ def run_evaluation(
 
 def summarise_results(results):
     label_results = {}
-    results_df = pd.DataFrame(results).set_index('filename')
+    results_df = pd.DataFrame(results).set_index("filename")
 
-    for label, df in results_df.groupby('label'):
-        most_common_mistake = df.loc[df['match'] == 'Incorrect']['predicted'].mode()
+    for label, df in results_df.groupby("label"):
+        most_common_mistake = df.loc[df["match"] == "Incorrect"]["predicted"].mode()
         try:
-            accuracy = df.match.value_counts().loc["Correct"] / df.match.value_counts().sum()
+            accuracy = (
+                df.match.value_counts().loc["Correct"] / df.match.value_counts().sum()
+            )
         except KeyError:
             accuracy = 0
 
         label_results[label] = {
             "accuracy": accuracy,
-            "correct_confidence": df.loc[df['match'] == 'Correct']['prob'].mean(),
-            "incorrect_confidence": df.loc[df['match'] == 'Incorrect']['prob'].mean(),
-            "common_mistake": most_common_mistake.loc[0] if len(most_common_mistake) > 0 else np.nan,
+            "correct_confidence": df.loc[df["match"] == "Correct"]["prob"].mean(),
+            "incorrect_confidence": df.loc[df["match"] == "Incorrect"]["prob"].mean(),
+            "common_mistake": (
+                most_common_mistake.loc[0] if len(most_common_mistake) > 0 else np.nan
+            ),
         }
 
     results_summary = pd.DataFrame(label_results).T
 
     return {
-        "accuracy": accuracy_score(results_df['label'], results_df['predicted']),
-        "precision": precision_score(results_df['label'], results_df['predicted'], average='weighted'),
-        "recall": recall_score(results_df['label'], results_df['predicted'], average='weighted'),
-        "f1": f1_score(results_df['label'], results_df['predicted'], average='weighted'),
+        "accuracy": accuracy_score(results_df["label"], results_df["predicted"]),
+        "precision": precision_score(
+            results_df["label"], results_df["predicted"], average="weighted"
+        ),
+        "recall": recall_score(
+            results_df["label"], results_df["predicted"], average="weighted"
+        ),
+        "f1": f1_score(
+            results_df["label"], results_df["predicted"], average="weighted"
+        ),
         "results_summary": results_summary,
     }
